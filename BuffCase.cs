984c004f64b63cdf5c5f4767c509ab75f0ae9191
Kim nam woo <namwoo321@gmail.com>
2026-05-21 10:48:06 +0900

    Rename Buff class and update methods

--- BuffCase.cs
+++ BuffCase.cs
@@ -4,42 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 
 /// <summary>
-/// BuffCase
-/// 이 파일은 웹 메이커에서 자동 생성되었습니다.
+/// BuffCase Auto Generated
 /// </summary>
 
-public class 이영G : Buff
+public class NewBuff : Buff
 {
     public override BuffType BuffType => BuffType.Good;
-    public override string Bname => "이영G";
+    public override string Bname => "NewBuff";
     public override string BnameKR => "새 버프";
     public override string Description => $"";
 
-    public 이영G(F_Cha target, F_Cha user, int s) : base(target, user, s) 
+    public NewBuff(F_Cha target, F_Cha user, int s) : base(target, user, s) 
     {
-    }
-
-    public override void OnActivate()
-    {
-        owner.Gain("Power", stack);
-    }
-
-    public override void OnUpdate(int val)
-    {
-        owner.Gain("Power", stack);
-    }
-
-    public override void OnDeactivate()
-    {
-        owner.Gain("Power", stack);
-    }
-
-    public override void OnTurnStart()
-    {
-        if (owner.CValue("SD") > 0)
-        {
-            owner.Gain("Power", stack);
-        }
     }
 
 }
+
