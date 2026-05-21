bc86fa6997e726e5abc5034ebe876c920f08529c
Kim nam woo <namwoo321@gmail.com>
2026-05-21 11:34:38 +0900

    Add BuffType property to Buff classes
    Updated Buff classes to include BuffType property.

--- BuffCS_Anima.cs
+++ BuffCS_Anima.cs
@@ -1,93 +1,79 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
-public class BuffCS_Anima
-{
-    // Anima 캐릭터 전용 버프들
-}
-
-
+/// <summary>
+/// BuffCase Auto Generated
+/// </summary>
 
 public class GPRand : Buff
 {
-    public override string Bname => "GP_Rand";
-    public override string Description => "[Anima] 자동생성 버프";
+    public override BuffType BuffType => BuffType.Good;
+    public override string Bname => "GPRand";
+    public override string Description => $"[Anima] 자동생성 버프";
+
     public GPRand(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-    
+
 }
 
 public class Explosion : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Explosion";
-    public override string Description => "[Anima] 자동생성 버프";
-    
+    public override string Description => $"[Anima] 자동생성 버프";
+
     public Explosion(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
 }
 
 public class GPY : Buff
 {
-    public override string Bname => "GP_Y";
-    public override string Description => "[Anima] 자동생성 버프";
-    
+    public override BuffType BuffType => BuffType.Good;
+    public override string Bname => "GPY";
+    public override string Description => $"[Anima] 자동생성 버프";
+
     public GPY(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
 }
 
 public class GPP : Buff
 {
-    public override string Bname => "GP_P";
-    public override string Description => "[Anima] 자동생성 버프";
-   
-    
+    public override BuffType BuffType => BuffType.Good;
+    public override string Bname => "GPP";
+    public override string Description => $"[Anima] 자동생성 버프";
+
     public GPP(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-    
 
-
-     
- 
-     
-    
 }
-
 
 public class GPR : Buff
 {
-    public override string Bname => "GP_R";
-    public override string Description => "[Anima] 자동생성 버프";
-    
+    public override BuffType BuffType => BuffType.Good;
+    public override string Bname => "GPR";
+    public override string Description => $"[Anima] 자동생성 버프";
+
     public GPR(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-    
 
-
-     
- 
-     
-    
 }
-
 
 public class GPSelf : Buff
 {
-    public override string Bname => "GP_Self";
-    public override string Description => "[Anima] 자동생성 버프";
-
+    public override BuffType BuffType => BuffType.Good;
+    public override string Bname => "GPSelf";
+    public override string Description => $"[Anima] 자동생성 버프";
 
     public GPSelf(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-}
 
+}
 
 public class LimitBreak : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "LimitBreak";
-    public override string Description => "[Anima] 자동생성 버프";
-   
-    
+    public override string Description => $"[Anima] 자동생성 버프";
+
     public LimitBreak(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-    
 
-
-     
- 
-     
-    
 }
+
