3f2acbf18d15c21a00a58ff9e54c2087b9f1dc47
Kim nam woo <namwoo321@gmail.com>
2026-05-21 16:04:12 +0900

    Refactor Buff classes in BuffCS_Anima.cs

--- BuffCS_Anima.cs
+++ BuffCS_Anima.cs
@@ -49,7 +49,7 @@ public class GPY : Buff
 
     public override void OnActivate()
     {
-        AddBuff(new GP(owner, caster,stack){Y += stack;});
+        AddBuff(new GP(owner, caster,stack){Y = stack;});
         owner.RemoveBuff(this);
     }
 
@@ -66,8 +66,7 @@ public class GPP : Buff
 
     public override void OnActivate()
     {
-        AddBuff(new GP(owner, caster,stack){P += stack;});
-        
+        AddBuff(new GP(owner, caster,stack){P = stack;});
         owner.RemoveBuff(this);
     }
 
@@ -84,8 +83,7 @@ public class GPR : Buff
 
     public override void OnActivate()
     {
-        AddBuff(new GP(owner, caster,stack){R += stack;});
-        
+        AddBuff(new GP(owner, caster,stack){R = stack;});
         owner.RemoveBuff(this);
     }
 
@@ -107,9 +105,9 @@ public class LimitBreak : Buff
     public override BuffType BuffType => BuffType.Good;
     public override string Bname => "LimitBreak";
     public override string BnameKR => "한계 해제";
-    public override string Description => $"의지 최대치 +1," +
-        $"\n공격, 피격, 폭발 피해 +25%," +
-        $"\n턴 종료 | 수치 +1, 수치가 3가 되면 기절, 버프 제거";
+    public override string Description => $"의지 최대치 1," +
+        $"\n공격, 피격, 폭발 피해 25%," +
+        $"\n턴 종료 | 수치 1, 수치가 3가 되면 기절, 버프 제거";
 
     public LimitBreak(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -123,9 +121,13 @@ public class LimitBreak : Buff
         owner.Gain("MaxWill", -1);
     }
 
-    public override void OnTurnEnd()
+    public override void OnTurnStart()
     {
-        stack++;
+        stack += 1;
+        if (stack == 4)
+        {
+            owner.RemoveBuff(this);
+        }
     }
 
     public override DamContext BeforeDamaged(DamContext DC)
@@ -151,10 +153,10 @@ public class GP : Buff
         $"\n예상 피해 {Red*6, Purple*5, Yellow*4}";
 
     public int R = 0;
-    public int P = 0;
-    public int Y = 0;
-    // public string[] GPC = new string[3]{null,null,null}
-    // 최대 크기 3인 string이거 만듬
+        public int P = 0;
+        public int Y = 0;
+        // public string[] GPC = new string[3]{null,null,null}
+        // 최대 크기 3인 string이거 만듬
 
     public GP(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -183,24 +185,25 @@ public class GP : Buff
     public override void OnUpdate(int val)
     {
         base.OnUpdate(val);
-         // 스택이 4 이상이면 폭발 발생. 같은 색일테지만 제대로 체크
-                if (stack > 3)
-                {
-                    stack -= 3;
-                    if (R>0)
-                    {
-                        owner.TakeDamage(3 * 7);
-                        owner.AddBuff(new Burn(owner, caster, 3));
-                        } else if (P > 0)
-                    {
-                        owner.TakeDamage(3 * 6);
-                        caster.AddBuff(new Power_1T(caster, caster, 3));
-                        } else if (Y > 0)
-                    {
-                        owner.TakeDamage(3 * 5);
-                        owner.AddBuff(new DefPower(owner, caster, 3));
-                    }
-                }
+        // 스택이 4 이상이면 폭발 발생. 같은 색일테지만 제대로 체크
+        if (stack > 3)
+        {
+            stack -= 3;
+            if (R>0)
+            {
+                owner.TakeDamage(3 * 7);
+                owner.AddBuff(new Burn(owner, caster, 3));
+                } else if (P > 0)
+            {
+                owner.TakeDamage(3 * 6);
+                caster.AddBuff(new Power_1T(caster, caster, 3));
+                } else if (Y > 0)
+            {
+                owner.TakeDamage(3 * 5);
+                owner.AddBuff(new DefPower(owner, caster, 3));
+            }
+        }
     }
 
 }
+
