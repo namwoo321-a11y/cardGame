0e21c39737ee046d6fe94681fc168df340489405
Kim nam woo <namwoo321@gmail.com>
2026-05-24 18:08:14 +0900

    Update BuffCS.cs

--- Buffs/BuffCS.cs
+++ Buffs/BuffCS.cs
@@ -11,31 +11,30 @@ public class Power : Buff
 {
     public override BuffType BuffType => BuffType.Power;
     public override string Bname => "Power";
-    public override string BnameKR => "Power";
-    public override string Description => $"가하는 피해량 증가";
-
-    // information
+    public override string BnameKR => "공격력";
+    public override string Description => $"가하는 피해 {Stack} 증가";
 
     public Power(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
+
+    // information
 
     public override void OnActivate()
     {
         // 힘 + stack
-        owner.Power += stack;
+        owner.Gain("Power", Stack);
     }
 
     public override void OnUpdate(int val)
     {
         base.OnUpdate(val);
-        // stack 업데이트
-        // 힘 + stack
+        owner.Gain("Power", val);
         owner.Power += val;
     }
 
     public override void OnDeactivate()
     {
         // 힘 -1
-        owner.Power -= stack;
+                owner.Power -= Stack;
     }
 
 }
@@ -44,31 +43,27 @@ public class DefPower : Buff
 {
     public override BuffType BuffType => BuffType.Bad;
     public override string Bname => "DefPower";
-    public override string BnameKR => "DefPower";
-    public override string Description => $"받는 피해 {stack} 증가," +
-        $"\n얻는 방어 {stack} 감소";
-
-    // information
+    public override string BnameKR => "방어력";
+    public override string Description => $"얻는 방어 {Stack} 감소";
 
     public DefPower(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
+    // information
+
     public override void OnActivate()
     {
-        // 힘 + stack
-        owner.DefPower += stack;
+        owner.Gain("DefPower", Stack);
     }
 
     public override void OnUpdate(int val)
     {
         base.OnUpdate(val);
-        // 힘 + stack
-        owner.DefPower += val;
+        owner.Gain("DefPower", val);
     }
 
     public override void OnDeactivate()
     {
-        // 힘 - stack
-        owner.DefPower -= stack;
+        owner.Consume("DefPower", Stack);
     }
 
 }
@@ -78,16 +73,16 @@ public class Power_1T : Buff
     public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Power_1T";
     public override string BnameKR => "Power_1T";
-    public override string Description => $"피해량 증가";
-
-    // information
+    public override string Description => $"피해량 {Stack} 증가 (1턴)";
 
     public Power_1T(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
+
+    // information
 
     public override void OnActivate()
     {
         // 마지막 부여자가 사용자.
-        owner.AddBuff(new Power(owner, caster, stack));
+        owner.AddBuff(new Power(owner, caster, Stack));
     }
 
     public override void OnUpdate(int val)
@@ -100,7 +95,7 @@ public class Power_1T : Buff
     public override void OnTurnStart()
     {
         // 마지막 부여자가 사용자.
-        owner.AddBuff(new Power(owner, caster, -stack));
+        owner.AddBuff(new Power(owner, caster, -Stack));
         // 효과 종료 시 버프 제거
         owner.RemoveBuff(this);
     }
@@ -112,11 +107,11 @@ public class PowerNT : Buff
     public override BuffType BuffType => BuffType.Good;
     public override string Bname => "PowerNT";
     public override string BnameKR => "PowerNT";
-    public override string Description => $"다음 턴 피해량 증가";
-
-    // information
+    public override string Description => $"다음 턴 피해량 {Stack} 증가";
 
     public PowerNT(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
+
+    // information
 
     public override void OnTurnStart()
     {
@@ -144,24 +139,24 @@ public class Poison : Buff
     public override BuffType BuffType => BuffType.Bad;
     public override string Bname => "Poison";
     public override string BnameKR => "독";
-    public override string Description => $"턴 종료 | 스택만큼 피해를 받고 1 감소";
-
-    // information
-        
-        
-        
-         // Poison 디버프.
+    public override string Description => $"턴 종료 | {Stack} 피해, 1 감소";
 
     public Poison(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
+
+    // information
+            
+            
+            
+             // Poison 디버프.
 
     public override void OnTurnEnd()
     {
         // 스택만큼 데미지
-        owner.TakeDamage(stack);
+        owner.TakeDamage(Stack);
         // 스택 1 감소
-        stack--;
+        Stack--;
         // 스택이 0이 되면 버프 제거
-        if (stack == 0) { owner.RemoveBuff(this); }
+                if (stack == 0) { owner.RemoveBuff(this); }
     }
 
 }
@@ -171,16 +166,15 @@ public class Burn : Buff
     public override BuffType BuffType => BuffType.Bad;
     public override string Bname => "Burn";
     public override string BnameKR => "화상";
-    public override string Description => $"턴 종료 | {stack} 피해, 1 감소";
-
-    // information
+    public override string Description => $"턴 종료 | {Stack} 피해, 1 감소";
 
     public Burn(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
     public override void OnTurnEnd()
     {
-        owner.TakeDamage(stack);
-        stack--; StackCheck();
+        owner.TakeDamage(Stack);
+        Stack -= 1;
+        StackCheck();
     }
 
 }
@@ -190,20 +184,13 @@ public class Bleed : Buff
     public override BuffType BuffType => BuffType.Bad;
     public override string Bname => "Bleed";
     public override string BnameKR => "Bleed";
-    public override string Description => $"턴 종료 | {stack} 피해, 제거";
-
-    // information
-        
-        
-        
-        
-         // 스택만큼 데미지
+    public override string Description => $"턴 종료 | {Stack} 피해, 제거";
 
     public Bleed(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
     public override void OnTurnEnd()
     {
-        owner.TakeDamage(stack);
+        owner.TakeDamage(Stack);
         owner.RemoveBuff(this);
     }
 
@@ -217,23 +204,17 @@ public class Hurt : Buff
     public override string Description => $"턴 시작 | 수치만큼 출혈 증가, 수치 1/2 감소" +
         $"\n피격 | 수치 1";
 
-    // information
-        
-        
-         // Hurt 디버프.
-
     public Hurt(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
     public override void OnTurnStart()
     {
-        if (stack > 0)
+        if (Stack> 0)
         {
             // 마지막 부여자가 사용자.
-            owner.AddBuff(new Bleed(owner, caster, stack));
+            owner.AddBuff(new Bleed(owner, caster, Stack));
             // 수치의 절반만큼 스택 감소")
-            owner.Consume(this, stack / 2);
-        }
-         else
+            owner.Consume(this, Stack/ 2);
+            } else
         {
             // 스택이 0이하가 되면 버프 제거
             owner.RemoveBuff(this);
@@ -255,8 +236,8 @@ public class Cold : Buff
 {
     public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Cold";
-    public override string BnameKR => "Cold";
-    public override string Description => "";
+    public override string BnameKR => "추위";
+    public override string Description => $"추위";
 
     public Cold(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -266,8 +247,8 @@ public class Freeze : Buff
 {
     public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Freeze";
-    public override string BnameKR => "Freeze";
-    public override string Description => "";
+    public override string BnameKR => "빙결_";
+    public override string Description => $"얼어붙은 상태.";
 
     public Freeze(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -277,8 +258,8 @@ public class Ice : Buff
 {
     public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Ice";
-    public override string BnameKR => "Ice";
-    public override string Description => "";
+    public override string BnameKR => "얼음";
+    public override string Description => $"얼어붙은 상태.";
 
     public Ice(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -288,9 +269,10 @@ public class Cure : Buff
 {
     public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Cure";
-    public override string BnameKR => "Cure";
-    public override string Description => "";
+    public override string BnameKR => "회복력";
+    public override string Description => $"턴 시작 | {Stack} 회복";
 
     public Cure(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
 }
+
