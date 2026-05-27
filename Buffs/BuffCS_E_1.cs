507d808ffb58c2430ba7618bff17705d3925131b
Kim nam woo <namwoo321@gmail.com>
2026-05-27 22:35:21 +0900

    Update BuffCS_E_1.cs

--- Buffs/BuffCS_E_1.cs
+++ Buffs/BuffCS_E_1.cs
@@ -1,35 +1,31 @@
-﻿using UnityEngine;
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
-public class BuffCS_E_1
-{
-    // E_1 캐릭터 전용 버프들
-}
-
-
+/// <summary>
+/// BuffCase Auto Generated - BuffCS_E_1.cs
+/// </summary>
 
 public class Spore : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Spore";
-    public override string Description => "[꽃개] 자동생성 버프";
-   
-    public Spore(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-    
-}
+    public override string BnameKR => "포자";
+    public override string Description => $"[꽃개] 자동생성 버프";
 
+    public Spore(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
+
+}
 
 public class Rooted : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Rooted";
-    public override string Description => "[덩굴] 자동생성 버프";
-   
+    public override string BnameKR => "속박";
+    public override string Description => $"[덩굴] 자동생성 버프";
 
-    
     public Rooted(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-    
 
-
-     
- 
-     
-    
 }
+
