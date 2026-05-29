bdea7909c76b803c4b5f4b052b2cd150d6c7e656
Kim nam woo <namwoo321@gmail.com>
2026-05-30 01:34:19 +0900

    Update BuffCS_Hana.cs

--- Buffs/BuffCS_Hana.cs
+++ Buffs/BuffCS_Hana.cs
@@ -1,43 +1,31 @@
-﻿using UnityEngine;
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
-public class BuffCS_Hana
-{
-    // Hana 캐릭터 전용 버프들
-}
-
-
+/// <summary>
+/// BuffCase Auto Generated - BuffCS_Hana.cs
+/// </summary>
 
 public class PowerUp : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "PowerUp";
-    public override string Description => "[Hana] 자동생성 버프";
-   
+    public override string BnameKR => "PowerUp";
+    public override string Description => $"[Hana] 자동생성 버프";
 
-    
     public PowerUp(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-    
 
-
-     
- 
-     
-    
 }
-
 
 public class CostUp : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "CostUp";
-    public override string Description => "[Hana] 자동생성 버프";
-   
+    public override string BnameKR => "CostUp";
+    public override string Description => $"[Hana] 자동생성 버프";
 
-    
     public CostUp(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-    
 
-
-     
- 
-     
-    
 }
+
