a657be7f8bc6a60af7e46d3d31543c6b2ec15117
Kim nam woo <namwoo321@gmail.com>
2026-05-30 01:33:55 +0900

    Update BuffCS_Hina.cs

--- Buffs/BuffCS_Hina.cs
+++ Buffs/BuffCS_Hina.cs
@@ -1,38 +1,29 @@
-﻿using System;
+using System;
 using UnityEngine;
-
-public class BuffCS_Hina
-{
-
-}
-
-#region Resources
+using System.Collections.Generic;
+using System.Linq;
 
 /// <summary>
-/// LP는 카드 효과가 모두 발동한 뒤 소모된다.
-/// 카드의 LP 효과 처리 후 자동으로 감소됨.
+/// BuffCase Auto Generated - BuffCS_Hina.cs
 /// </summary>
+
 public class LP : Buff
 {
+    public override BuffType BuffType => BuffType.Resource;
     public override string Bname => "LP";
     public override string BnameKR => "LP";
     public override string Description => $"자원";
-    public override BuffType BuffType => BuffType.Resource;
-
 
     public LP(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
+
 }
 
-#endregion
-
-#region 호르몬 버프
-
-/// <summary> 페닐에틸아민 - 수치당 방어 +1, 힘 +1, 수치-- </summary>
 public class Phenylethylamine : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Phenylethylamine";
+    public override string BnameKR => "Phenylethylamine";
     public override string Description => $"방어 +{Stack}, 힘 +{Stack}";
-   
 
     public Phenylethylamine(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -41,6 +32,7 @@ public class Phenylethylamine : Buff
         owner.AddBuff(new DefPower(owner, caster, Stack));
         owner.AddBuff(new Power(owner, caster, Stack));
     }
+
     public override void OnUpdate(int val)
     {
         base.OnUpdate(val);
@@ -54,13 +46,15 @@ public class Phenylethylamine : Buff
         owner.AddBuff(new Power(owner, caster, Stack));
         owner.RemoveBuff(this);
     }
+
 }
-/// <summary>도파민 - 턴 시작 | 코스트 + 수치, 수치--</summary>
+
 public class Dopamine : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Dopamine";
+    public override string BnameKR => "Dopamine";
     public override string Description => $"턴 시작시 코스트 +{Stack}\n이후 제거됨";
-   
 
     public Dopamine(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -69,48 +63,53 @@ public class Dopamine : Buff
         owner.Will += Stack;
         owner.RemoveBuff(this);
     }
+
 }
-/// <summary>옥시토신 - 턴 시작 | LP + 수치, 수치--</summary>
+
 public class Oxytocin : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Oxytocin";
+    public override string BnameKR => "Oxytocin";
     public override string Description => $"턴 시작 | LP +{Stack}";
-   
 
     public Oxytocin(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-        
+
     public override void OnTurnStart()
     {
         owner.AddBuff(new LP(owner, caster, Stack));
     }
+
 }
 
-
-//// <summary>코르티솔 - 턴 시작 | 체력 피해 3 x 수치, 슬픔 +1 | 이후 1/2 </summary>
 public class Cortisol : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Cortisol";
+    public override string BnameKR => "Cortisol";
     public override string Description => $"턴 시작 | 체력 피해 3 x 수치, 슬픔 +1" +
         $"\n1/2 감소";
+
     public Cortisol(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
+
     public override void OnTurnStart()
     {
         owner.TakeDamage(Stack * 3);
-
         if (owner is Ally al)
         {
             if (al.HasCValue("Depress")) { al.AddBuff(new Depress(owner, caster, 1)); }
         }
         Stack /= 2; StackCheck();
     }
+
 }
-/// <summary> 엔케팔린 - 턴 시작 | 카드 수치만큼 뽑음, 기쁨 +1 | 이후 1/2 </summary>
+
 public class Enkephalin : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Enkephalin";
     public override string BnameKR => "엔케팔린";
-    public override string Description => $"턴 시작시 카드 {Stack}장 뽑음,
-       $"\n의지 +1, 수치 -1, 기쁨 +1" +
+    public override string Description => $"의지 +1, 수치 -1, 기쁨 +1" +
         $"\n1/2 감소";
 
     public Enkephalin(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
@@ -125,28 +124,30 @@ public class Enkephalin : Buff
         }
         Stack--; if (Stack <= 0) { owner.RemoveBuff(this); }
     }
+
 }
-/// <summary>스테로이드 - 턴 종료 | 회복 4 x 수치, 수치 -1, 분노 +1</summary>
+
 public class Steroid : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Steroid";
+    public override string BnameKR => "Steroid";
     public override string Description => $"턴 종료시 체력 4 x {Stack} 회복, 수치 -1\n(지속)" +
         $"\n1/2 감소";
+
     public Steroid(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
-    public override BuffType BuffType => BuffType.Good;
+
     public override void OnTurnEnd()
     {
         owner.Heal(Stack * 4);
         Stack--; StackCheck(); // [수치--, 0 제거]
     }
+
 }
 
-
-// 인지 세트
-
-/// <summary> 인지 약화 - 행동 [방어, 공격] | - 수치 *10%, 행동 -2 </summary>
 public class CogDecline : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "CogDecline";
     public override string BnameKR => "인지 저하";
     public override string Description => $"다음 행동 능률 -{Stack*10}%, 2 소모." +
@@ -154,32 +155,32 @@ public class CogDecline : Buff
 
     public CogDecline(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
+    public override void OnTurnEnd()
+    {
+        Stack--; if (Stack <= 0) { owner.RemoveBuff(this); }
+    }
+
     public override DamContext BeforeAttack(DamContext DC)
     {
         // 자신에게 피해 입히기
         if (DC.IsPreview)
         {
             float reduction = Stack / 10f; // 행동 능률 감소 (예: Stack=1이면 10% 감소)
-
-            DC.PercentDamage *= reduction; // 행동 효과 반감
-        }
-        else
+                        DC.PercentDamage *= reduction; // 행동 효과 반감
+        } else
         {
             float reduction = Stack / 10f; // 행동 능률 감소 (예: Stack=1이면 10% 감소)
-            DC.PercentDamage *= reduction; // 행동 효과 반감
-            Stack /= 2; if (Stack <= 0) { owner.RemoveBuff(this); }
+                        DC.PercentDamage *= reduction; // 행동 효과 반감
+                        Stack /= 2; if (Stack <= 0) { owner.RemoveBuff(this); }
         }
         return DC;
     }
-    public override void OnTurnEnd()
-    {
-        Stack--; if (Stack <= 0) { owner.RemoveBuff(this); }
-    }
+
 }
-#endregion
-/// <summary> 인지 강화 - 행동 [방어, 공격] | + 수치 *10%, 행동 +2 </summary>
+
 public class CogIncrease : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "CogIncrease";
     public override string BnameKR => "인지 증가";
     public override string Description => $"행동 능률 +{Stack}%" +
@@ -189,15 +190,12 @@ public class CogIncrease : Buff
 
 }
 
-
-    #region Status Effects
-
-    /// <summary>다음 턴 카드 추가 드로우 LP 충전 γ 카드에서 부여됨 </summary>
 public class NextDraw : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "NextDraw";
+    public override string BnameKR => "NextDraw";
     public override string Description => $"다음 턴 카드 수치만큼 추가 드로우\n턴 시작시 발동 후 제거";
-   
 
     public NextDraw(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -209,15 +207,15 @@ public class NextDraw : Buff
         }
         owner.RemoveBuff(this);
     }
+
 }
 
-/// <summary> 기절 - 행동 불가</summary>
 public class Stun : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Stun";
+    public override string BnameKR => "Stun";
     public override string Description => $"수치만큼 턴동안 기절 | 행동 불가";
-
-
 
     public Stun(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -229,13 +227,14 @@ public class Stun : Buff
             owner.RemoveBuff(this);
         }
     }
+
 }
 
-
-/// <summary> 무작위 호르몬 즉시 부여 (긴급 호르몬 투여)</summary>
 public class RHormone : Buff
 {
-    public override string Bname => "R_Hormone";
+    public override BuffType BuffType => BuffType.Good;
+    public override string Bname => "RHormone";
+    public override string BnameKR => "RHormone";
     public override string Description => $"수치만큼 무작위 호르몬 부여";
 
     public RHormone(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
@@ -243,35 +242,31 @@ public class RHormone : Buff
     public override void OnTurnStart()
     {
         // 무작위로 감정 선택: 0=기쁨, 1=우울, 2=분노, 3=울분
-        int randomEmotion = UnityEngine.Random.Range(0, 4);
+                int randomEmotion = UnityEngine.Random.Range(0, 4);
         switch (randomEmotion)
         {
             case 0: // 기쁨
-                owner.AddBuff(new Joy(owner, caster, Stack));
-                break;
+            owner.AddBuff(new Joy(owner, caster, Stack));
+            break;
             case 1: // 우울
-                owner.AddBuff(new Depress(owner, caster, Stack));
-                break;
+            owner.AddBuff(new Depress(owner, caster, Stack));
+            break;
             case 2: // 분노
-                owner.AddBuff(new Anger(owner, caster, Stack));
-                break;
+            owner.AddBuff(new Anger(owner, caster, Stack));
+            break;
             case 3: // 울분
-                owner.AddBuff(new Distress(owner, caster, Stack));
-                break;
+            owner.AddBuff(new Distress(owner, caster, Stack));
+            break;
         }
     }
+
 }
 
-#endregion
-
-
-
-#region Emotions (감정)
-
-/// <summary>기쁨 - 힘, 방어 n 증가 감정 폭발(5 이상): 황홀경 - 코스트 드로우 +2</summary>
 public class Joy : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Joy";
+    public override string BnameKR => "Joy";
     public override string Description => $"수치만큼 기쁨 | 힘/방어 +수치\n(5 이상) 황홀경: 코스트 드로우 +2";
 
     public Joy(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
@@ -303,17 +298,16 @@ public class Joy : Buff
             al.DrawCard(2); // 황홀경: 코스트 드로우 +2
         }
     }
+
 }
 
-/// <summary>우울 - 힘 -n, 방어력 +n/2감정 폭발(5 이상): 절망 - 피해량 -50%</summary>
 public class Depress : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Depress";
     public override string BnameKR => "우울";
     public override string Description => $"힘 - 수치/2 ({Stack / 2}), 방어 + 수치 ({Stack})" +
         $"\n";
-
-
 
     public Depress(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -334,7 +328,6 @@ public class Depress : Buff
 
     public override void OnDeactivate()
     {
-        owner.RemoveBuff(this);
     }
 
     private void CheckEmoExplosion()
@@ -345,15 +338,16 @@ public class Depress : Buff
             owner.RemoveBuff(this);
         }
     }
+
 }
 
-/// <summary>분노 - 방어력 n, 힘 +n/2감정 폭발(5 이상): 야수화 - 공격시 자해</summary>
 public class Anger : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Anger";
+    public override string BnameKR => "Anger";
     public override string Description => $"방어 -{Stack}, 힘 +{Stack / 2}" +
         $"\n10 이상이면 감정 폭발 - 야수화";
-
 
     public Anger(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -374,7 +368,6 @@ public class Anger : Buff
 
     public override void OnDeactivate()
     {
-        owner.RemoveBuff(this);
     }
 
     private void CheckEmoExplosion()
@@ -384,15 +377,15 @@ public class Anger : Buff
             owner.AddBuff(new Berserk(owner, caster, 1));
         }
     }
+
 }
 
-/// <summary>울분 - 힘, 방어력 n 감소 감정 폭발(5 이상): 과부하 - 기절, 행동 취소</summary>
 public class Distress : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Distress";
+    public override string BnameKR => "Distress";
     public override string Description => $"수치만큼 울분 | 힘/방어 -수치\n(5 이상) 과부하: 기절, 행동 취소";
-
-
 
     public Distress(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -413,7 +406,6 @@ public class Distress : Buff
 
     public override void OnDeactivate()
     {
-        owner.RemoveBuff(this);
     }
 
     private void CheckEmoExplosion()
@@ -423,23 +415,22 @@ public class Distress : Buff
             owner.AddBuff(new Stun(owner, caster, 1)); // 과부하: 기절
         }
     }
+
 }
 
-#endregion
-
-
-#region Emotion Explosions (감정 폭발)
-
-/// <summary>절망 - 우울 감정 폭발 (5 이상) 피해량 -50%
-/// </summary>
 public class Despair : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Despair";
+    public override string BnameKR => "Despair";
     public override string Description => $"절망 상태\n피해량 -50% (감정 폭발)";
 
-
-
     public Despair(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
+
+    public override void OnTurnEnd()
+    {
+        owner.RemoveBuff(this);
+    }
 
     public override DamContext BeforeAttack(DamContext DC)
     {
@@ -447,48 +438,37 @@ public class Despair : Buff
         return DC;
     }
 
+}
+
+public class Berserk : Buff
+{
+    public override BuffType BuffType => BuffType.Good;
+    public override string Bname => "Berserk";
+    public override string BnameKR => "Berserk";
+    public override string Description => $"야수화 상태\n공격시 자해 (감정 폭발)";
+
+    public Berserk(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
+
     public override void OnTurnEnd()
     {
         owner.RemoveBuff(this);
     }
-}
-
-/// <summary>
-/// 야수화 - 분노 감정 폭발 (5 이상)
-/// 공격시 자해
-/// </summary>
-public class Berserk : Buff
-{
-    public override string Bname => "Berserk";
-    public override string Description => $"야수화 상태\n공격시 자해 (감정 폭발)";
-
-
-
-    public Berserk(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
     public override void AfterAttack(DamContext DC)
     {
         // 자신이 입힌 피해가 저장된 DC. 무슨 효과 가져오기
-        // 자신에게 피해 입히기
+                // 자신에게 피해 입히기
         owner.TakeDamage(Stack);
     }
 
-    public override void OnTurnEnd()
-    {
-        owner.RemoveBuff(this);
-    }
 }
 
-/// <summary>
-/// 과부하 - 울분 감정 폭발 (5 이상)
-/// 기절, 행동 취소 (Stun과 동일)
-/// </summary>
 public class Overload : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Overload";
+    public override string BnameKR => "Overload";
     public override string Description => $"과부하 상태\n기절, 행동 취소 (감정 폭발)";
-
-
 
     public Overload(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -496,17 +476,15 @@ public class Overload : Buff
     {
         owner.RemoveBuff(this);
     }
+
 }
 
-/// <summary>
-/// 황홀경 - 기쁨 감정 폭발 (5 이상)
-/// 코스트 드로우 +2 (이미 Joy에서 처리됨)
-/// </summary>
 public class Ecstasy : Buff
 {
+    public override BuffType BuffType => BuffType.Good;
     public override string Bname => "Ecstasy";
+    public override string BnameKR => "Ecstasy";
     public override string Description => $"황홀경 상태\n코스트 드로우 +2 (감정 폭발)";
-  
 
     public Ecstasy(F_Cha target, F_Cha user, int s) : base(target, user, s) { }
 
@@ -514,6 +492,6 @@ public class Ecstasy : Buff
     {
         owner.RemoveBuff(this);
     }
+
 }
 
-#endregion
